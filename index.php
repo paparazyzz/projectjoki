c107f319e5267da85f571d217417cd542fb53012
paparazyzz <willykrizer@gmail.com>
2024-06-11 12:22:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Waterbom | Bali</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+</head>
+<body>
+    <!-- Bagian Header -->
+     <header>
+        <a href="" class="logo"> Waterbom <span>Bali</span></a>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#facilities">Facilities</a></li>
+            <li><a href="">Location</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+        </ul>
+     </header>
+     <!-- Section Home -->
+      <section class="home" id="home">
+        <div class="home-text">
+            <h1>Welcome to<br>Waterboom <span>Bali</span></h1>
+            <p>Visit us for more experience fun family holiday <br> Completely Your Journey !!!</p>
+            <a href="" class="home-btn"> Get Location
+            <i class='bx bx-current-location' style='color:#ffffff'></i>
+            </a>
+        </div>
+      </section>
+      <!-- Section Facility -->
+       <section class="container" id="facilities">
+        <div class="text">
+            <h2>Our Facilities</h2>
+            <p>Get started vacation with our service</p>
+        </div>
+        <div class="row-items">
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="restaurant36.png">
+                </div>
+                <h4>Restaurant</h4>
+                <p>Ready any local food of Indonesia</p>
+            </div>
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="home36.png">
+                </div>
+                <h4>Gazebos</h4>
+                <p>These private retreats for families</p>
+            </div>
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="spa36.png">
+                </div>
+                <h4>Spa & Massage</h4>
+                <p>Replenish your energy</p>
+            </div>
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="first36.png">
+                </div>
+                <h4>Aid Clinic</h4>
+                <p>Ready first aid clinic</p>
+            </div>
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="store36.png">
+                </div>
+                <h4>Local Store</h4>
+                <p>Sell all merchandise and amnities</p>
+            </div>
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="church36.png">
+                </div>
+                <h4>Mosque and Churc</h4>
+                <p>Ready a prayer room for guest</p>
+            </div>
+        </div>
+       </section>
+       <!-- Section Card -->
+        <section class="card">
+            <div class="text">
+                <h2>Our Package</h2>
+                <p>Ready promo for this week!</p>
+            </div>
+            <div class="cards">
+                <div class="card-shadow">
+                    <ul>
+                        <li class="pack">Single Day Pass</li>
+                        <li id="sdp" class="price-bottom">Rp 387,500</li>
+                        <li class="bottom-bar">1 day access</li>
+                        <li class="bottom-bar">1 person only</li>
+                        <li class="bottom-bar">Free swimming</li>
+                        <li>
+                            <button class="btn">Learn More 
+                            <i class='bx bx-chevron-down' style='color:rgba#ffffff'></i>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-active">
+                    <ul>
+                        <li class="pack">Duo Pass</li>
+                        <li id="sdp" class="price-top">Rp 1,400,000</li>
+                        <li class="bottom-bar">1 day access</li>
+                        <li class="bottom-bar">2 access person</li>
+                        <li class="bottom-bar">Free swimming</li>
+                        <li class="bottom-bar">Inc towel & locker</li>
+                        <li class="bottom-bar">Free 30min massage</li>
+                        <li>
+                            <button class="btn">Learn More 
+                            <i class='bx bx-chevron-down' style='color:rgba#ffffff'></i>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-shadow">
+                    <ul>
+                        <li class="pack">Two Day Pass</li>
+                        <li id="sdp" class="price-bottom">Rp 645,000</li>
+                        <li class="bottom-bar">2 day access</li>
+                        <li class="bottom-bar">1 person only</li>
+                        <li class="bottom-bar">Free swimming</li>
+                        <li>
+                            <button class="btn">Learn More
+                            <i class='bx bx-chevron-down' style='color:rgba#ffffff'></i>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!-- Contact Section -->
+    <!-- Sambung ke JavaScript -->
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
